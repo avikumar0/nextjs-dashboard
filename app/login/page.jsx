6c908b3235f7e4e74a3a229d3ad98698decc68f6
avikumar0 <avikumar5955@gmail.com>
2024-04-07 13:49:48 +0530

    Add new files and update existing files for UI improvements

---
+++ app/login/page.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "@/app/ui/dashboard/login/loginForm/loginForm.jsx";
+const LoginPage = () => {
+  return (
+    <div className="width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
